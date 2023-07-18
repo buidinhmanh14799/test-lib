ecccd3ad3b52ea9d27e5d5ee5d019dfad6940ae2
buidinhmanh <buidinhmanh14799@gmail.com>
2023-07-18 13:48:45 +0700

    chore: initial commit

---
+++ android/src/main/java/com/testlib/TestLibModule.java
@@ -0,0 +1,32 @@
+package com.testlib;
+
+import androidx.annotation.NonNull;
+
+import com.facebook.react.bridge.Promise;
+import com.facebook.react.bridge.ReactApplicationContext;
+import com.facebook.react.bridge.ReactContextBaseJavaModule;
+import com.facebook.react.bridge.ReactMethod;
+import com.facebook.react.module.annotations.ReactModule;
+
+@ReactModule(name = TestLibModule.NAME)
+public class TestLibModule extends ReactContextBaseJavaModule {
+  public static final String NAME = "TestLib";
+
+  public TestLibModule(ReactApplicationContext reactContext) {
+    super(reactContext);
+  }
+
+  @Override
+  @NonNull
+  public String getName() {
+    return NAME;
+  }
+
+
+  // Example method
+  // See https://reactnative.dev/docs/native-modules-android
+  @ReactMethod
+  public void multiply(double a, double b, Promise promise) {
+    promise.resolve(a * b);
+  }
+}
